3acc6dcdf4446822c7e1104cc8625ceffe261f42
maddi-stinks <740546@student.dusd.net>
2025-10-29 11:02:21 -0700

    finished variables and functions
    finished

---
+++ P4FloresScriptingProject/Assets/Scripts/VariabllesAnd Functions.cs
@@ -0,0 +1,38 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariabllesAndFunctions : MonoBehaviour
+{
+    //create my integer variable
+    int myInt = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //run  my variable through my function 
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //create the multiply by two function
+    int MultiplyByTwo(int number)
+    {
+        //create local variable 
+        int result;
+
+        //multiply the num,ber  by two
+        result = number * 2;
+
+        //return the value
+        return result;
+    }
+
+}
