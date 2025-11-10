e50bcbc488abe76048893ef882ac6a161d73136a
maddi-stinks <740546@student.dusd.net>
2025-11-10 11:28:12 -0800

    finished Conventions and Syntaxs
    started If Statements

--- P4FloresScriptingProject/Assets/Scripts/ConventionsAndZyntax.cs
+++ P4FloresScriptingProject/Assets/Scripts/ConventionsAndZyntax.cs
@@ -8,16 +8,20 @@ public class ConventionsAndCintax : MonoBehaviour
     void Start()
     {
         //this  line is there to tell me the x position of my object
+        
+
+        /*Hi there!
+         * this is two lines!
+         * */
+
+
+        // Update is called once per frame
         Debug.Log(transform.position.x);
 
+        if (transform.position.y <= 5f)
+        {
+            Debug.Log ("I'm about to hit the ground!");
 
-    }
-
-    // Update is called once per frame
-    void Update()
-
-        if (Transform.position.y <=  5f)
-    {
-        Debug.Log("I'm about to hit the ground!");
+        }
     }
 }
