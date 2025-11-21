bd039c82da38b9b9be888c372c1b597620021c35
maddi-stinks <740546@student.dusd.net>
2025-11-21 10:38:06 -0800

    finished Loop Scripts
    not fully tested, just scripts

--- P4FloresScriptingProject/Assets/Scripts/Loops/DoWhileLoop.cs
+++ P4FloresScriptingProject/Assets/Scripts/Loops/DoWhileLoop.cs
@@ -5,21 +5,14 @@ using UnityEngine;
 
 public class DoWhileLoop : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         bool shouldContinue = false;
-         
+
         do
-    }
+        {
+            print("Hello World");
 
-    // Update is called once per frame
-    void Update()
-    {
-
-        print("Hello World");
-
-    
-        }while(shouldContinue == true);
+        } while (shouldContinue == true);
     }
 }
